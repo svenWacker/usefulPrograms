bc8c68a7490e2f9d77844e34d69ef4ca2cafe11b
svenWacker <s.u.wacker@outlook.de>
2021-02-17 14:26:57 +0100

    math and word update

--- mathGames.js
+++ mathGames.js
@@ -3,3 +3,46 @@ const reverse = (num) => num.toString().split("").reverse().join("");
 console.log(reverse(34532));
 console.log(reverse(-12345));
 console.log();
+
+console.log("---*unique*---");
+const unique = (arr) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let currentValue = arr[i];
+    if (arr.indexOf(arr[i]) == arr.lastIndexOf(arr[i])) {
+      result.push(currentValue);
+    }
+  }
+  return result.join(", ");
+};
+console.log(unique([0, 0, 0.77, 0, 0]));
+console.log(unique([0, 1, 1, 1, 1, 1, 1, 1]));
+console.log(unique([3, 3, 3, 7, 3, 3]));
+console.log();
+
+console.log("---*findGreatest*---");
+const findGreatest = (arr, num) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let element = arr[i];
+    if (element > num) {
+      result.push(element);
+    }
+  }
+  return result; //.join(", ") string
+};
+console.log(findGreatest([3, 4, 5], 4));
+console.log(findGreatest([10, 20, 30], 12));
+console.log(findGreatest([0, 10, 3], 4));
+console.log();
+
+console.log("---**Is it Symmetrical?**---");
+const isSymmetrical = (num) => {
+  return num.toString() === num.toString().split("").reverse().join("");
+};
+console.log(isSymmetrical(7227)); // ➞ true
+console.log(isSymmetrical(12567)); // ➞ false
+console.log(isSymmetrical(44444444)); // ➞ true
+console.log(isSymmetrical(9939)); // ➞ false
+console.log(isSymmetrical(1112111)); // ➞ true
+console.log();
---
+++ wordGames.js
@@ -0,0 +1,84 @@
+console.log("---*alphaOrder*---");
+const alphaOrder = (word) => {
+  return word.split("").sort().join("");
+};
+console.log(alphaOrder("webdev"));
+console.log();
+
+console.log("---*toCamelCase*---");
+const toCamelCase = (str) => {
+  let strToArr = str.toLowerCase().split("_");
+  for (let i = 1; i < strToArr.length; i++) {
+    strToArr[i] = strToArr[i][0].toUpperCase() + strToArr[i].slice(1);
+  }
+  return strToArr.join("");
+};
+console.log(toCamelCase("hello_world_see"));
+console.log(toCamelCase("javascript_is_fun"));
+console.log();
+
+console.log("---*hackerSpeak*---");
+const hackerSpeak = (str) => {
+  const lowerStr = str.toLowerCase();
+  const letters = lowerStr.split("");
+  const hackerSpeak = [];
+  // const superCleanStr = cleanStr.replaceAll(" ", "");
+  for (let i = 0; i < letters.length; i++) {
+    switch (letters[i]) {
+      case "a":
+        letters[i] = "4";
+        break;
+      case "e":
+        letters[i] = "3";
+        break;
+      case "i":
+        letters[i] = "1";
+        break;
+      case "o":
+        letters[i] = "0";
+        break;
+      case "s":
+        letters[i] = "5";
+        break;
+      default:
+        letters[i];
+    }
+    hackerSpeak.push(letters[i]);
+  }
+  return hackerSpeak.join("");
+};
+console.log(hackerSpeak("javascript is cool"));
+console.log(hackerSpeak("programming is fun"));
+console.log(hackerSpeak("become a coder"));
+console.log();
+
+const pigLatin = (str) => {
+  const lowerCase = str.toLowerCase().replaceAll(".", "");
+  const splitStr = lowerCase.split(" ");
+  const vowels = ["a", "e", "i", "o", "u"];
+  const pigTranslation = [];
+  for (let i = 0; i < splitStr.length; i++) {
+    let firstChar = splitStr[i][0];
+    let remainder = splitStr[i].slice(1, splitStr[i].length);
+    if (vowels.includes(firstChar)) {
+      // checking if i is truthy or falsy.
+      //If falsy, then it's the first word because it's 0.
+      let firstCharVowel =
+        (i ? firstChar : firstChar.toUpperCase()) + remainder + "way";
+      pigTranslation.push(firstCharVowel);
+    } else {
+      if (i === 0) {
+        let firstCharRem = remainder[0];
+        let wordRemainder = remainder.slice(1, remainder.length);
+        remainder = firstCharRem.toUpperCase() + wordRemainder;
+      }
+      let firstCharConsonant = remainder + firstChar + "ay";
+      pigTranslation.push(firstCharConsonant);
+    }
+  }
+
+  return pigTranslation.join(" ") + ".";
+};
+console.log(pigLatin("Cats are great pets."));
+console.log(pigLatin("Tom got a small piece of pie."));
+console.log(pigLatin("He told us a very exciting tale."));
