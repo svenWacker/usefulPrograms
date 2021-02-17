1566461111f560cd000dd3bf6615fc2af610474a
svenWacker <s.u.wacker@outlook.de>
2021-02-17 14:17:19 +0100

    mathGames added

---
+++ mathGames.js
@@ -0,0 +1,5 @@
+console.log("---*reverse number*---");
+const reverse = (num) => num.toString().split("").reverse().join("");
+console.log(reverse(34532));
+console.log(reverse(-12345));
+console.log();
